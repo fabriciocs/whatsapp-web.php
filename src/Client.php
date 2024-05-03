832f703e349d38ef3f7aece3ddd079ce3258bc24
Fabrício da Cunha Santos <fabriciodacunhasantos@gmail.com>
2024-05-02 22:03:15 -0300

    Add PHP version of index.js

---
+++ src/Client.php
@@ -0,0 +1,12 @@
+<?php
+namespace WhatsappWeb;
+
+class Client {
+    public function on($sevent, callback) { 
+        // Event handling logic for 'qr', 'ready', 'message', etc.
+    }
+
+    public function initialize() {
+        // Initialization logic
+    }
+}
